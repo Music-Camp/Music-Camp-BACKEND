ffc8b6a162cd22d2c6115cd0d6b580e9df4a3dc0
이승주 <112889287+WinsomeJoo@users.noreply.github.com>
2024-05-11 22:14:48 +0900

    Revert "Feat/comment 기능구현"

--- src/main/java/com/MusicCamp/backend/post/dto/PostAllResDto.java
+++ src/main/java/com/MusicCamp/backend/post/dto/PostAllResDto.java
@@ -1,11 +1,8 @@
 package com.MusicCamp.backend.post.dto;
 
-import com.MusicCamp.backend.likes.Likes;
 import com.MusicCamp.backend.post.Post;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
-import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -13,24 +10,14 @@ public class PostAllResDto {
     private Long postId;
     private Long userId;
     private String nickname;
-    private String title;
-    private Long likesNum;
+    public  String title;
 
     public static PostAllResDto of(Post post){
-        List<Likes> likesList = post.getLikesList();
-        long count= 0;
-        for(Likes likes:likesList){
-            if(likes.getIsStatus()==true){
-                count++;
-            }
-        }
-
         return new PostAllResDto(
                 post.getId(),
                 post.getUser().getId(),
                 post.getUser().getNickname(),
-                post.getTitle(),
-                count
+                post.getTitle()
                 );
     }
 
